8f5e2c7b61ed85891cc2b7a0299180cd6a68124a
Miguel Angel <maceari2707@g.educaand.es>
2023-02-09 12:52:46 +0100

    apuntes_typescript

--- typescript/apuntes.ts
+++ typescript/apuntes.ts
@@ -1,1 +1,35 @@
-ejemplo
+let color: string = "rojo";
+let edad: number = 2;
+
+let listado: number[] = [1,2,3,4]; //Array
+
+let nuevoListado: [string,number] = ["miguel angel",12] //Tupla
+
+let noSeguro: any = 3; //Usamos any cuando no tenemos conocimiento de nuestra estructura de datos
+noSeguro = "Se puede cambiar el tipo"; 
+let lista: any[] = [1,"pepe",true];
+
+class Persona{
+     nombre: string;
+     edad: number;
+    //igual que en java
+}
+
+let p1 = new Persona(); //creamos el objeto p1 de la clase persona
+p1.nombre = "aa";
+interface direccion{
+    calle: string;
+    num: number;
+    poblacion: number;
+    //se usan para crear objetos especificos para definir un objeto de datos
+}
+
+function sumar (sumando1: number, sumando2:number){
+    return sumando1 + sumando2;
+        //hara exactamente lo mismo que en java
+}
+
+function nombreCompleto (nombre: string, apellido?:string){
+    //hara exactamente lo mismo que en java solo que el parametro apellido es opcional
+}
+
